8c4ca751e561dadce9d4e190a40168d96ea6a80e
softdeveloper23 <brannongarrett77@gmail.com>
2024-10-10 14:41:20 +0200

    Updated code with input and output

--- src/main/java/encoder/CipherEncoder.java
+++ src/main/java/encoder/CipherEncoder.java
@@ -3,9 +3,22 @@ package encoder;
 import java.util.Scanner;
 
 public class CipherEncoder {
-    System.out.println("Input string:")
-    Scanner scanner = new Scanner(System.in);
-    String inputString = scanner.nextLine();
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
 
+        System.out.println("Input string:");
+        Scanner scanner = new Scanner(System.in);
+        String original = scanner.nextLine();
 
+        for (int i = 0; i < original.length(); i++) {
+            sb.append(original.charAt(i));
+            if (i != original.length() - 1) {
+                sb.append(' ');
+            }
+        }
+
+        String result = sb.toString();
+        System.out.println(result);
+        scanner.close();
+    }
 }
