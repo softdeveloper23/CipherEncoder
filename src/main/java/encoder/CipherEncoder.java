6488d0a96bf7897624744f5d7ff4ea16cf7a787f
softdeveloper23 <brannongarrett77@gmail.com>
2024-10-14 14:26:06 +0200

    Finished coding the converter

--- src/main/java/encoder/CipherEncoder.java
+++ src/main/java/encoder/CipherEncoder.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class CipherEncoder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Input string:");
         String input = scanner.nextLine();
         char[] charArray = input.toCharArray();
+        System.out.println("The result:");
         convertToBinary(charArray);
         scanner.close();
     }
 
     private static void convertToBinary(char[] arr) {
+        String result;
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            result = Integer.toBinaryString(arr[i]);
+            String formattedBinaryString = String.format("%7s", result).replace(' ', '0');
+            System.out.println(arr[i] + " = " + formattedBinaryString);
         }
     }
 }
